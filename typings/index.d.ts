62def62fee9695eb5e22ae58daaa4726d16851ef
Danny Rankevich <dannyr@similarweb.com>
2016-11-01 16:36:02 +0200

    update versions

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,3 +1,5 @@
 /// <reference path="globals/angular/index.d.ts" />
 /// <reference path="globals/es6-shim/index.d.ts" />
 /// <reference path="globals/jquery/index.d.ts" />
+/// <reference path="globals/react-dom/index.d.ts" />
+/// <reference path="globals/react/index.d.ts" />
